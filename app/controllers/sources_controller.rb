4ef667836be581cd4ec0747e90dbcd7bfeb02d5c
Scott Duncombe <srduncombe@gmail.com>
2012-06-18 01:53:56 -0700

    Adding cron controller

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -60,6 +60,27 @@ class SourcesController < ApplicationController
     end
   end
   
+  def import_all # Controller for cron imports
+    fresh_sources = Source.all.reject{ |s| s.events.future.length < 1 } # Could add some random element here to make sure we're not hitting all the sources
+    errors = []
+
+    fresh_sources.each do |source|
+      begin
+        source.create_events!
+      rescue
+        # Could have more robust error hadndling here
+        errors.push( { :source => source, :error => 'hey there was an error' } )
+      end
+    end
+    
+    if errors.length < 1
+      render :text => '', :layout => false #Render nothing if cron is succesful 
+    else
+      render :text => errors, :layout => false #Should come up some something better if it fails
+    end
+        
+  end
+  
   # GET /sources
   # GET /sources.xml
   def index
